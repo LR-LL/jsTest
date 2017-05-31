2787e2629ccb1c31ea76888a8f514dbdf0578865
LR <1020251822@qq.com>
2017-05-31 11:43:30 +0800

    added function_BST and exam10_2/3

---
+++ function_BST.js
@@ -0,0 +1,101 @@
+function node(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+    this.show = show;
+}
+
+function BST() {
+    this.root = null;
+    this.insert = insert;
+    this.inOrder = inOrder;
+    this.preOrder = preOrder;
+    this.postOrder = postOrder;
+    this.getMin = getMin;
+    this.getMax = getMax;
+    this.find = find;
+}
+
+function show() {
+    return this.data;
+}
+
+function insert(data) {
+    var newNode = new node(data, null, null);
+    if (this.root == null) {
+        this.root = newNode;
+    } else {
+        var current = this.root;
+        var parent;
+        while (true) {
+            parent = current;
+            if (data < current.data) {
+                current = current.left;
+                if (current == null) {
+                    parent.left = newNode;
+                    break;
+                }
+            } else {
+                current = current.right;
+                if (current == null) {
+                    parent.right = newNode;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function inOrder(node) {
+    if (!(node == null)) {
+        inOrder(node.left);
+        console.log(node.show() + ' ');
+        inOrder(node.right);
+    }
+}
+
+function preOrder(node) {
+    if (!(node == null)) {
+        console.log(node.show() + ' ');
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+}
+
+function postOrder(node) {
+    if (!(node == null)) {
+        postOrder(node.left);
+        postOrder(node.right);
+        console.log(node.show() + ' ');
+    }
+}
+
+function getMin() {
+    var current = this.root;
+    while (current.left != null) {
+        current = current.left;
+    }
+    return current.data;
+}
+
+function getMax() {
+    var current = this.root;
+    while (current.right != null) {
+        current = current.right;
+    }
+    return current.data;
+}
+
+function find(data) {
+    var current = this.root;
+    while (current) {
+        if (data < current.data) {
+            current = current.left;
+        } else if (data > current.data) {
+            current = current.right;
+        } else {
+            return true; //找到了返回true
+        }
+    }
+    return false; //没找到返回false
+}
