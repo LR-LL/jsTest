b366a2d95a8d73e3040312639be4bdfc030d1167
LR <1020251822@qq.com>
2017-06-26 15:36:14 +0800

    added function_CArry.js and examBubbleSort.js

---
+++ examSort12-3.js
@@ -0,0 +1,63 @@
+function CArray(numElements) {
+    this.dataStore = [];
+    this.pos = 0;
+    this.numElements = numElements;
+    this.insert = insert;
+    this.toString = toString;
+    this.clear = clear;
+    this.setData = setData;
+    this.swap = swap;
+    for (var i = 0; i < numElements; i++) {
+        this.dataStore[i] = i;
+    }
+    this.bubbleSort=bubbleSort;
+}
+function setData(){
+  for(var i=0;i<this.numElements;i++){
+    this.dataStore[i]=Math.floor(Math.random()*(this.numElements+1));
+  }
+}
+function clear(){
+  for(var i=0;i<this.dataStore.length;i++){
+    this.dataStore[i]=0;
+  }
+}
+function insert(element){
+  this.dataStore[this.pos++]=element;
+}
+function toString(){
+  var restr='';
+  for(var i=0;i<this.dataStore.length;i++){
+    restr+=this.dataStore[i]+' ';
+    if(i>0&i%20==0){
+      restr+='\n';
+    }
+  }
+  return restr;
+}
+function swap(arr,index1,index2){
+  var temp=arr[index1];
+  arr[index1]=arr[index2];
+  arr[index2]=temp;
+}
+function bubbleSort(){
+  var num=this.dataStore.length;
+  var flag;//对冒泡排序进行优化，当没有次序变化时不再循环
+  for(var outer=num;outer>=2;outer--){
+    flag=0;
+    for(var inner=0;inner<=num-1;inner++){
+      if(this.dataStore[inner]>this.dataStore[inner+1]){
+        this.swap(this.dataStore,inner,inner+1);
+        flag=1;
+      }
+    }
+    console.log(this.dataStore);
+    if(flag==0){
+      break;
+    }
+  }
+}
+var arr=new CArray(20);
+arr.setData();
+console.log(arr.dataStore);
+arr.bubbleSort();
